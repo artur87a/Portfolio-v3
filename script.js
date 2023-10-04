eee25e7527f24ac343c3dcb3200416c7ec9d0377
artur.kodehode@gmail.com <artur.kodehode@gmail.com>
2023-10-04 12:50:28 +0200

    v4

--- script.js
+++ script.js
@@ -3,13 +3,19 @@
             var content = document.getElementById("main");
             var hamburgerIcon = document.querySelector(".hamburger-icon");
 
-            if (sidebar.style.width === "250px") {
+            if (sidebar.style.width === "150px") {
                 sidebar.style.width = "0";
                 content.style.marginLeft = "0";
                 hamburgerIcon.classList.remove("change");
             } else {
-                sidebar.style.width = "250px";
-                content.style.marginLeft = "250px";
+                sidebar.style.width = "150px";
+                content.style.marginLeft = "150px";
                 hamburgerIcon.classList.add("change");
             }
         }
+
+
+
+
+
+    
